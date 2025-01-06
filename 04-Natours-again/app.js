b66f6b72f444b39737ffc835c9e585c716f0398c
MhideTech <okefolahanolamide2006@gmail.com>
2025-01-06 09:11:20 +0100

    handled patch request

--- 04-Natours-again/app.js
+++ 04-Natours-again/app.js
@@ -62,6 +62,23 @@ app.post('/api/v1/tours', (req, res) => {
   );
 });
 
+app.patch('/api/v1/tours/:id', (req, res) => {
+  const id = req.params.id * 1;
+  const tour = tours.find((el) => el.id === id);
+
+  if (!tour) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID',
+    });
+  }
+
+  res.status(200).json({
+    status: 'success',
+    message: '<Updated tour here... />',
+  });
+});
+
 const port = 3000;
 app.listen(port, '127.0.0.1', () => {
   console.log(`Server is running on port ${port}...`);
