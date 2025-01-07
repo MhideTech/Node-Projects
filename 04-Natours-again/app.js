b32b9ebb8e8eaacd72dd1260192d3294eaade86e
MhideTech <okefolahanolamide2006@gmail.com>
2025-01-07 07:11:15 +0100

    used third party middleware: morgan

--- 04-Natours-again/app.js
+++ 04-Natours-again/app.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const fs = require('fs');
+const morgan = require('morgan');
 
 const app = express();
 
+// 1) MIDDLEWARES
+app.use(morgan('dev', ));
 app.use(express.json());
 
 app.use((req, res, next) => {
@@ -14,6 +17,7 @@ const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
 
+// 2) ROUTE HANDLERS
 const getAllTours = (req, res) => {
   console.log(req.requestTime);
 
@@ -107,6 +111,7 @@ const deleteTour = (req, res) => {
 // app.patch('/api/v1/tours/:id', updateTour);
 // app.delete('/api/v1/tours/:id', deleteTour);
 
+// 3) ROUTES
 app.route('/api/v1/tours').get(getAllTours).post(createTour);
 app
   .route('/api/v1/tours/:id')
@@ -114,6 +119,7 @@ app
   .patch(updateTour)
   .delete(deleteTour);
 
+// 4) START SERVER
 const port = 3000;
 app.listen(port, '127.0.0.1', () => {
   console.log(`Server is running on port ${port}...`);
