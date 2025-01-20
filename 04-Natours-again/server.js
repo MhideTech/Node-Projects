db2bb4570832367221a68d25f04c2c45e13de1d0
MhideTech <okefolahanolamide2006@gmail.com>
2025-01-20 16:58:32 +0100

    created a simple tour model

--- 04-Natours-again/server.js
+++ 04-Natours-again/server.js
@@ -17,6 +17,24 @@ mongoose
   })
   .then(() => console.log('DB Connection successful'));
 
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour must have a name'],
+    unique: true
+  },
+  rating: {
+    type: Number,
+    default: 4.5
+  },
+  price: {
+    type: Number,
+    default: [true, 'A tour must have a price']
+  }
+});
+
+const Tour = mongoose.model('Tour', tourSchema);
+
 const app = require('./app');
 // console.log(process.env);
 
