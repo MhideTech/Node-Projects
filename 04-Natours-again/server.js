2e679e5ff1980efe1a285403d36c72f0092dc909
MhideTech <okefolahanolamide2006@gmail.com>
2025-01-20 11:42:42 +0100

    connected database with express app

--- 04-Natours-again/server.js
+++ 04-Natours-again/server.js
@@ -3,7 +3,19 @@ const mongoose = require('mongoose');
 
 dotenv.config({ path: './config.env' });
 
-const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.PASSWORD);
+const DB = process.env.DATABASE.replace(
+  '<db_password>',
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true
+  })
+  .then(() => console.log('DB Connection successful'));
 
 const app = require('./app');
 // console.log(process.env);
