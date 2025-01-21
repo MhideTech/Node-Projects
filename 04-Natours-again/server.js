957dcc400d8911f3ae871060e693a138fd6d714b
MhideTech <okefolahanolamide2006@gmail.com>
2025-01-21 23:18:53 +0100

    refactored using MVC

--- 04-Natours-again/server.js
+++ 04-Natours-again/server.js
@@ -17,38 +17,6 @@ mongoose
   })
   .then(() => console.log('DB Connection successful'));
 
-const tourSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, 'A tour must have a name'],
-    unique: true
-  },
-  rating: {
-    type: Number,
-    default: 4.5
-  },
-  price: {
-    type: Number,
-    default: [true, 'A tour must have a price']
-  }
-});
-
-const Tour = mongoose.model('Tour', tourSchema);
-
-const testTour = new Tour({
-  name: 'The Park Camper',
-  price: 997
-});
-
-testTour
-  .save()
-  .then(doc => {
-    console.log(doc);
-  })
-  .catch(err => {
-    console.log('ERROR:', err);
-  });
-
 const app = require('./app');
 // console.log(process.env);
 
