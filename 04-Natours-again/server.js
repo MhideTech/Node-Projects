35abf24acf8e103c86c8deba5370053404e9b2bf
MhideTech <okefolahanolamide2006@gmail.com>
2025-01-21 11:22:20 +0100

    created documents and tested the models

--- 04-Natours-again/server.js
+++ 04-Natours-again/server.js
@@ -35,6 +35,20 @@ const tourSchema = new mongoose.Schema({
 
 const Tour = mongoose.model('Tour', tourSchema);
 
+const testTour = new Tour({
+  name: 'The Park Camper',
+  price: 997
+});
+
+testTour
+  .save()
+  .then(doc => {
+    console.log(doc);
+  })
+  .catch(err => {
+    console.log('ERROR:', err);
+  });
+
 const app = require('./app');
 // console.log(process.env);
 
