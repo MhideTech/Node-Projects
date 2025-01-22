71b439595999ea96071d3fabf3021c802b313010
MhideTech <okefolahanolamide2006@gmail.com>
2025-01-22 16:01:43 +0100

    implemented create tour controller

--- 04-Natours-again/controllers/tourController.js
+++ 04-Natours-again/controllers/tourController.js
@@ -1,21 +1,11 @@
 const Tour = require('./../models/tourModel');
 
-exports.checkBody = (req, res, next) => {
-  if (!req.body.name || !req.body.price) {
-    return res.status(400).json({
-      status: 'fail',
-      message: 'Name and price needs to be provided'
-    });
-  }
-  next();
-};
-
 exports.getAllTours = (req, res) => {
   console.log(req.requestTime);
 
   res.status(200).json({
     status: 'success',
-    requestedAt: req.requestTime,
+    requestedAt: req.requestTime
     // results: tours.length,
     // data: {
     //   tours
@@ -25,8 +15,7 @@ exports.getAllTours = (req, res) => {
 
 exports.getTour = (req, res) => {
   // console.log(req.params);
-
-  const id = req.params.id * 1;
+  // const id = req.params.id * 1;
   // const tour = tours.find(el => el.id === id);
   // res.status(200).json({
   //   status: 'success',
@@ -36,13 +25,18 @@ exports.getTour = (req, res) => {
   // });
 };
 
-exports.createTour = (req, res) => {
-  res.status(201).json({
-    status: 'success'
-    // data: {
-    //   tour: newTour
-    // }
-  });
+exports.createTour = async (req, res) => {
+  try {
+    const newTour = await Tour.create(req.body);
+    res.status(201).json({
+      status: 'success',
+      data: {
+        tour: newTour
+      }
+    });
+  } catch (err) {
+    res.status(400).json({ status: 'fail', message: 'Invalid data sent' });
+  }
 };
 
 exports.updateTour = (req, res) => {
--- 04-Natours-again/server.js
+++ 04-Natours-again/server.js
@@ -20,7 +20,7 @@ mongoose
 const app = require('./app');
 // console.log(process.env);
 
-const port = process.env.PORT || 3000;
+const port = 3000;
 
 app.listen(port, '127.0.0.1', () => {
   console.log(`Server is running on port ${port}...`);
