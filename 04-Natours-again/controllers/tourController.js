aea5cc8e1f954173ca75393c4cd53dc2f8bbdce1
MhideTech <okefolahanolamide2006@gmail.com>
2025-01-22 17:12:24 +0100

    implemented updating document functionality on updateTour handler

--- 04-Natours-again/controllers/tourController.js
+++ 04-Natours-again/controllers/tourController.js
@@ -22,7 +22,7 @@ exports.getAllTours = async (req, res) => {
 exports.getTour = async (req, res) => {
   try {
     const tour = await Tour.findById(req.params.id);
-    console.log(req.params);
+
     res.status(200).json({
       status: 'success',
       data: {
@@ -48,11 +48,22 @@ exports.createTour = async (req, res) => {
   }
 };
 
-exports.updateTour = (req, res) => {
-  res.status(200).json({
-    status: 'success',
-    message: '<Updated tour here... />'
-  });
+exports.updateTour = async (req, res) => {
+  try {
+    const tour = await Tour.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true
+    });
+
+    res.status(200).json({
+      status: 'success',
+      data: {
+        tour
+      }
+    });
+  } catch (err) {
+    res.status(400).json({ status: 'fail', message: err });
+  }
 };
 
 exports.deleteTour = (req, res) => {
