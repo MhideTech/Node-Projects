1060a494927cd5e7b30e280acd670adf54c49ad5
MhideTech <okefolahanolamide2006@gmail.com>
2025-02-15 21:05:00 +0100

    added new field to tour model schema

--- 04-Natours-again/models/tourModel.js
+++ 04-Natours-again/models/tourModel.js
@@ -46,6 +46,7 @@ const tourSchema = new mongoose.Schema({
     required: [true, 'A tour must have a cover image']
   },
   images: [String],
+  startDates: [Date],
   createdAt: {
     type: Date,
     default: Date.now(),
